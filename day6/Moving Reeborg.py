5ee422a0f02ded7804d080b0df451ad5c7cd5c6b
olga-perepichka <107623605+olga-perepichka@users.noreply.github.com>
2026-02-28 11:35:26 +0100

    Adding tasks for day 6

---
+++ day6/Moving Reeborg.py
@@ -0,0 +1,31 @@
+'''
+Here is the link to the task and playground:
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+Copy the code below and see if the Reeborg can jump over the hurdles and reach the goal.
+You can also try to modify the code and see if you can make it more efficient or easier to read.
+'''
+
+'''
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while not right_is_clear():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+'''
